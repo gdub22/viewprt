83f62b592d11d8811e39ee591d6ee20dca14fa71
Ivan Nikolić <niksy5@gmail.com>
2021-05-04 17:07:57 +0200

    Add support for proper ES module

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,33 @@
+const path = require('path')
+const { promises: fs } = require('fs')
 const buble = require('rollup-plugin-buble')
 const replace = require('rollup-plugin-replace')
 const { terser: minify } = require('rollup-plugin-terser')
 
 const ENV = process.env.NODE_ENV
+
+const writePackageType = () => {
+  return {
+    name: 'package-type',
+    async writeBundle(output) {
+      let prefix, type
+      if (output.file.includes('umd/')) {
+        prefix = 'dist/umd'
+        type = 'commonjs'
+      } else if (output.file.includes('es/')) {
+        prefix = 'dist/es'
+        type = 'module'
+      }
+      if (typeof prefix !== 'undefined') {
+        const package_ = path.join(prefix, 'package.json')
+        try {
+          await fs.unlink(package_)
+        } catch (error) {}
+        await fs.writeFile(package_, JSON.stringify({ type }), 'utf8')
+      }
+    }
+  }
+}
 
 const baseConfig = {
   input: 'src/index.js',
@@ -10,7 +35,12 @@ const baseConfig = {
     name: 'viewprt',
     sourcemap: true
   },
-  plugins: [buble(), replace({ 'process.env.NODE_ENV': JSON.stringify(ENV) }), ENV === 'production' && minify()]
+  plugins: [
+    writePackageType(),
+    buble(),
+    replace({ 'process.env.NODE_ENV': JSON.stringify(ENV) }),
+    ENV === 'production' && minify()
+  ]
 }
 
 export default [
