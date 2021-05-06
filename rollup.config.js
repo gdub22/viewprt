dff2657e44ae3aeca1a9dddbfc836ccb0826a1fb
Ivan Nikolić <niksy5@gmail.com>
2021-05-06 09:10:02 +0200

    Use `.mjs` extension for ES module

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,8 @@
-const path = require('path')
-const { promises: fs } = require('fs')
 const buble = require('rollup-plugin-buble')
 const replace = require('rollup-plugin-replace')
 const { terser: minify } = require('rollup-plugin-terser')
 
 const ENV = process.env.NODE_ENV
-
-const writePackageType = () => {
-  return {
-    name: 'package-type',
-    async writeBundle(output) {
-      let prefix, type
-      if (output.file.includes('umd/')) {
-        prefix = 'dist/umd'
-        type = 'commonjs'
-      } else if (output.file.includes('es/')) {
-        prefix = 'dist/es'
-        type = 'module'
-      }
-      if (typeof prefix !== 'undefined') {
-        const package_ = path.join(prefix, 'package.json')
-        try {
-          await fs.unlink(package_)
-        } catch (error) {}
-        await fs.writeFile(package_, JSON.stringify({ type }), 'utf8')
-      }
-    }
-  }
-}
 
 const baseConfig = {
   input: 'src/index.js',
@@ -35,12 +10,7 @@ const baseConfig = {
     name: 'viewprt',
     sourcemap: true
   },
-  plugins: [
-    writePackageType(),
-    buble(),
-    replace({ 'process.env.NODE_ENV': JSON.stringify(ENV) }),
-    ENV === 'production' && minify()
-  ]
+  plugins: [buble(), replace({ 'process.env.NODE_ENV': JSON.stringify(ENV) }), ENV === 'production' && minify()]
 }
 
 export default [
@@ -57,7 +27,7 @@ export default [
     output: {
       ...baseConfig.output,
       format: 'esm',
-      file: 'dist/es/viewprt.js'
+      file: 'dist/es/viewprt.mjs'
     }
   }
 ]
